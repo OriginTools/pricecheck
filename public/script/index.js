d413144234f52c893d84cade3e3bdb27f45cbdab
obfuscatedgenerated <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-04-27 20:54:49 +0100

    minor mobile fixups

--- public/script/index.js
+++ public/script/index.js
@@ -3,3 +3,7 @@ var params = new URLSearchParams(window.location.search);
 if (params.has("item")) {
     document.querySelector("#item").value = params.get("item");
 }
+
+if (document.activeElement != document.querySelector("#item")) { // fix for mobile's refusal to autofocus
+    document.querySelector("#item").focus();
+}
